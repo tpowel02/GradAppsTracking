9c0c3949a2555e9fd3e5fc627fa2167cb7d897f6
Phil Gammill <gritzmunkey@gmail.com>
2015-03-02 17:31:15 -0600

    Revert "test"
    This reverts commit d8df1cb39b47f076097489ccef4c6cb4ba84d391.

---
+++ App_Code/DB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DB
+{
+    // database information
+
+    private static SqlConnection GetConnection()
+    {
+        // database connection string
+
+        var connectionString = new SqlConnectionStringBuilder
+        {
+            DataSource = "",
+            UserID = "",
+            Password = ""
+        };
+
+        var connection = new SqlConnection(connectionString.ToString());
+
+        connection.Open();
+
+        return connection;
+    }
+}
